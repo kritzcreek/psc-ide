b494c27da3f61105853a8d88ed04bb1fc66dd559
creek <christoph.hegemann@gmx.net>
2015-09-10 15:52:38 +0200

    adds JSON representation for Completion
    also changes the underlying representation to a newtype

--- lib/PureScript/Ide/CodecJSON.hs
+++ lib/PureScript/Ide/CodecJSON.hs
@@ -2,6 +2,9 @@ module PureScript.Ide.CodecJSON where
 
 import PureScript.Ide.Externs (ExternDecl(..))
 import Data.Aeson
+import Data.Text (Text())
+import Data.Text.Lazy (toStrict, fromStrict)
+import Data.Text.Lazy.Encoding (decodeUtf8, encodeUtf8)
 
 instance ToJSON ExternDecl where
   toJSON (FunctionDecl n t)        = object ["name" .= n, "type" .= t]
@@ -9,3 +12,9 @@ instance ToJSON ExternDecl where
   toJSON (DataDecl     n t)        = object ["name" .= n, "type" .= t]
   toJSON (Dependency   n names)    = object ["module" .= n, "names" .= names]
   toJSON (FixityDeclaration f p n) = object ["name" .= n, "fixity" .= show f, "precedence" .= p]
+
+encodeT :: (ToJSON a) => a -> Text
+encodeT = toStrict . decodeUtf8 . encode
+
+decodeT :: (FromJSON a) => Text -> Maybe a
+decodeT = decode . encodeUtf8 . fromStrict
