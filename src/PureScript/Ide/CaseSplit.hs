3107461022048325471111ecd78ce41c84b42bd6
Christoph Hegemann <christoph.hegemann1337@gmail.com>
2016-01-16 19:52:55 +0100

    accepts type parameters when casesplitting

--- src/PureScript/Ide/CaseSplit.hs
+++ src/PureScript/Ide/CaseSplit.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE RecordWildCards  #-}
 {-# LANGUAGE TemplateHaskell  #-}
 {-# LANGUAGE DataKinds        #-}
+{-# LANGUAGE LambdaCase       #-}
 
 
 module PureScript.Ide.CaseSplit where
@@ -11,7 +12,7 @@ import           Control.Concurrent.STM
 import           "monad-logger" Control.Monad.Logger
 import           Control.Monad.Reader
 import           Control.Monad.Except
--- import Control.Lens
+import Data.List (find)
 import qualified Data.Map                    as M
 import           Data.Monoid
 import           Data.Text                   (Text)
@@ -21,91 +22,78 @@ import           Language.PureScript.Names
 import           Language.PureScript.Types
 import           Language.PureScript.Pretty
 import           Language.PureScript.Environment
-import           PureScript.Ide.Completion
-import           PureScript.Ide.State
+import           Language.PureScript.Parser.Types
+import           Language.PureScript.Parser.Lexer (lex)
+import           Language.PureScript.Parser.Common (runTokenParser)
+import           Prelude hiding (lex)
 import           PureScript.Ide.Error
 import           PureScript.Ide.Types hiding (Type)
+import           Text.Parsec as P
 
-type Constructor = (Text, [(Text, Text)])
+type Constructor = (ProperName 'ConstructorName, [Type])
 
-getExternFiles :: (PscIde m, MonadIO m) =>
+getExternFiles :: (PscIde m) =>
                   m (M.Map ModuleName ExternsFile)
 getExternFiles = do
   stateVar <- envStateVar <$> ask
-  liftIO $ externsFiles <$> readTVarIO stateVar
+  liftIO (externsFiles <$> readTVarIO stateVar)
 
-findConstructors :: (PscIde m, MonadLogger m, MonadError PscIdeError m) =>
-                    Text -> m [ExternsDeclaration]
-findConstructors q = do
-  m <- findModuleForDatatype q
-  ef' <- M.lookup m <$> getExternFiles
-  case ef' of
-    Nothing -> do
-      $(logDebug) $ "Couldn't find ExternFile for query: " <> q
-      throwError . NotFound $
-        "Couldn't find Module for query: " <>
-        q <> " Did you maybe forget to load it?"
-    Just ef -> pure $ findConstructors' (ProperName (T.unpack q)) ef
-
-findConstructors' :: ProperName 'TypeName -> ExternsFile -> [ExternsDeclaration]
-findConstructors' pn ef = filter doesConstruct (efDeclarations ef)
-  where doesConstruct EDDataConstructor{..} = pn == edDataCtorTypeCtor
-        doesConstruct _ = False
+isEDType :: ExternsDeclaration -> Bool
+isEDType (EDType _ _ _) = True
+isEDType _ = False
 
 caseSplit :: (PscIde m, MonadLogger m, MonadError PscIdeError m) =>
              Text -> m [Constructor]
 caseSplit q = do
-  ctors <- findConstructors q
-  pure (map mkCtor ctors)
+  (tc, args) <- splitTypeConstructor (parseType' (T.unpack q))
+  (EDType _ _ (DataType typeVars ctors)) <- findTypeDeclaration tc
+  let applyTypeVars = everywhereOnTypes (replaceAllTypeVars (zip (map fst typeVars) args))
+  let appliedCtors = map (\(n, ts) -> (n, map applyTypeVars ts)) ctors
+  pure appliedCtors
 
-mkCtor :: ExternsDeclaration -> Constructor
-mkCtor EDDataConstructor{..} = (T.pack $ runProperName edDataCtorName, args)
+{- ["EDType {
+     edTypeName = ProperName {runProperName = \"Either\"}
+   , edTypeKind = FunKind Star (FunKind Star Star)
+   , edTypeDeclarationKind =
+       DataType [(\"a\",Just Star),(\"b\",Just Star)]
+                [(ProperName {runProperName = \"Left\"},[TypeVar \"a\"])
+                ,(ProperName {runProperName = \"Right\"},[TypeVar \"b\"])]}"]
+-}
+
+findTypeDeclaration :: (PscIde m, MonadLogger m, MonadError PscIdeError m) =>
+                         ProperName 'TypeName -> m ExternsDeclaration
+findTypeDeclaration q = do
+  efs <- getExternFiles
+  let m = getAlt $ foldMap (findTypeDeclaration' q) efs
+  case m of
+    Just mn -> pure mn
+    Nothing -> throwError (GeneralError "Not Found")
+
+findTypeDeclaration' ::
+  ProperName 'TypeName
+  -> ExternsFile
+  -> Alt Maybe ExternsDeclaration
+findTypeDeclaration' t ExternsFile{..} =
+  Alt $ find (\case
+            EDType tn _ _ -> tn == t
+            _ -> False) efDeclarations
+
+splitTypeConstructor :: (MonadError PscIdeError m) =>
+                        Type -> m (ProperName 'TypeName, [Type])
+splitTypeConstructor = go []
   where
-    args = map (\t -> ("_", T.strip . T.pack . prettyPrintType $ t)) ts
-    (ts, _) = splitType edDataCtorType
-mkCtor _ = error "lulz"
-
-findModuleForDatatype :: (PscIde m, MonadIO m, MonadLogger m, MonadError PscIdeError m) =>
-                         Text -> m ModuleName
-findModuleForDatatype q = do
-  matches <- getExactMatches q [] <$> getAllModulesWithReexports
-  case matches of
-    [] -> throwError . NotFound $ "Could not find a module for query: " <> q
-    (Completion (mn, _, _):_) -> pure (moduleNameFromString (T.unpack mn))
-
-splitType :: Type -> ([Type], Type)
-splitType t = (arguments, returns)
-  where
-    returns = last splitted
-    arguments = init splitted
-    splitted = splitType' t
-    splitType' (ForAll _ t' _) = splitType' t'
-    splitType' (TypeApp (TypeApp t' lhs) rhs)
-          | t' == tyFunction = lhs : splitType' rhs
-    splitType' t' = [t']
-
-makeCases ::
-  Text -> -- ^ function name
-  [Text] -> -- ^ arguments so far
-  Int -> -- ^ The argument to split on
-  [Constructor] -> -- ^ the cases to cover
-  [Text]
-makeCases fName args n =
-  map makeLine
-  where
-    argsl = take n args
-    argsr = drop (n+1) args
-    makeLine ctor = fName <> " " <> T.unwords (argsl ++ [prettyCtor ctor] ++ argsr) <> " = ?" <> fName
+    go acc (TypeApp ty arg) = go (arg : acc) ty
+    go acc (TypeConstructor tc) = pure (disqualify tc, acc)
+    go _ _ = throwError (GeneralError "Failed to read TypeConstructor")
 
 prettyCtor :: Constructor -> Text
-prettyCtor (ctorName, []) = ctorName
-prettyCtor (ctorName, ctorArgs) = "("<> ctorName <> " " <> T.unwords (map prettyCtorArg ctorArgs) <>")"
+prettyCtor (ctorName, []) = T.pack (runProperName ctorName)
+prettyCtor (ctorName, ctorArgs) =
+  "("<> T.pack (runProperName ctorName) <> " "
+  <> T.unwords (map prettyCtorArg ctorArgs) <>")"
 
-prettyCtorArg :: (Text, Text) -> Text
-prettyCtorArg (s, t) = "(" <> s <> " :: " <> t <> ")"
-
-prettyCtorArgWithoutType :: (Text, Text) -> Text
-prettyCtorArgWithoutType (s, _) = s
+prettyCtorArg :: Type -> Text
+prettyCtorArg t = "( _ :: " <> T.pack (prettyPrintTypeAtom t) <> ")"
 
 makePattern ::
   Text -> -- ^ current line
@@ -116,3 +104,9 @@ makePattern ::
 makePattern t x y = makePattern' (T.take x t) (T.drop y t)
   where
     makePattern' lhs rhs = map (\ctor -> lhs <> prettyCtor ctor <> rhs)
+
+parseType' :: String -> Type
+parseType' s = let (Right t) = do
+                     ts <- lex "" s
+                     runTokenParser "" (parseType <* P.eof) ts
+               in t
